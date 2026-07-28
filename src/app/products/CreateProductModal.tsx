32e2f7535ad356dabf952ef7ef8562503e9010b7
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2026-07-28 22:45:49 +0700

    update

--- src/app/products/CreateProductModal.tsx
+++ src/app/products/CreateProductModal.tsx
@@ -11,13 +11,13 @@ type ProductFormData = {
 
 type CreateProductModalProps = {
   isOpen: boolean;
-  onclose: () => void;
+  onClose: () => void;
   onCreate: (formData: ProductFormData) => void;
 };
 
 const CreateProductModal = ({
   isOpen,
-  onclose,
+  onClose,
   onCreate,
 }: CreateProductModalProps) => {
   const [formData, setFormData] = useState({
@@ -42,7 +42,7 @@ const CreateProductModal = ({
   const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
     onCreate(formData);
-    onclose();
+    onClose();
   };
 
   if (!isOpen) return null;
@@ -52,22 +52,92 @@ const CreateProductModal = ({
     "block w-full mb-2 p-2 border-gray-500 border-2 rounded-md";
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
-      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-        <Header name="Create New Product" />
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="productName" className={labelCssStyles}>
+    <div className='fixed inset-0 bg-gray-600/50 overflow-y-auto h-full w-full z-20'>
+      <div className='relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white'>
+        <Header name='Create New Product' />
+        <form
+          onSubmit={handleSubmit}
+          className='mt-5'
+        >
+          {/* PRODUCT NAME */}
+          <label
+            htmlFor='productName'
+            className={labelCssStyles}
+          >
             Product Name
           </label>
           <input
-            type="text"
-            name="name"
-            placeholder="Name"
+            type='text'
+            name='name'
+            placeholder='Name'
             onChange={handleChange}
             value={formData.name}
             className={inputCssStyles}
             required
           />
+          {/* PRICE */}
+          <label
+            htmlFor='productPrice'
+            className={labelCssStyles}
+          >
+            Price
+          </label>
+          <input
+            type='number'
+            name='price'
+            placeholder='Price'
+            onChange={handleChange}
+            value={formData.price}
+            className={inputCssStyles}
+            required
+          />
+          {/* STOCK QUANTITY */}
+          <label
+            htmlFor='stockQuantity'
+            className={labelCssStyles}
+          >
+            Stock Quantity
+          </label>
+          <input
+            type='number'
+            name='stockQuantity'
+            placeholder='Stock Quantity'
+            onChange={handleChange}
+            value={formData.stockQuantity}
+            className={inputCssStyles}
+            required
+          />
+          {/* RATING */}
+          <label
+            htmlFor='rating'
+            className={labelCssStyles}
+          >
+            Rating
+          </label>
+          <input
+            type='number'
+            name='rating'
+            placeholder='Rating'
+            onChange={handleChange}
+            value={formData.rating}
+            className={inputCssStyles}
+            required
+          />
+
+          {/* CREATE ACTION */}
+          <button
+            type='submit'
+            className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'
+          >
+            Create
+          </button>
+          <button
+            onClick={onClose}
+            type='button'
+            className='ml-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700'
+          >
+            Cancel
+          </button>
         </form>
       </div>
     </div>
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,17 @@
 "use client";
-import { useGetProductsQuery } from "@/state/api";
+import { useCreateProductMutation, useGetProductsQuery } from "@/state/api";
 import React, { useState } from "react";
 import Header from "../(components)/Header";
 import { PlusCircleIcon, SearchIcon } from "lucide-react";
 import Rating from "../(components)/Rating";
+import CreateProductModal from "./CreateProductModal";
+
+type ProductFormData = {
+  name: string;
+  price: number;
+  stockQuantity: number;
+  rating: number;
+};
 
 const Products = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -14,27 +22,35 @@ const Products = () => {
     isError,
   } = useGetProductsQuery(searchTerm);
 
+  const [createProduct] = useCreateProductMutation();
+  const handleCreateProduct = async (productData: ProductFormData) => {
+    await createProduct(productData);
+  };
+
   if (isLoading) {
-    return <div className="py-4">Loading...</div>;
+    return <div className='py-4'>Loading...</div>;
   }
 
   if (isError || !products) {
     return (
-      <div className="text-center text-red-500 py-4">
+      <div className='text-center text-red-500 py-4'>
         Failed to fetch products
       </div>
     );
   }
 
   return (
-    <div className="mx-auto pb-5 w-full flex flex-col" style={{ maxHeight: "100vh", overflow: "hidden" }}>
+    <div
+      className='mx-auto pb-5 w-full flex flex-col'
+      style={{ maxHeight: "100vh", overflow: "hidden" }}
+    >
       {/* SEARCH BAR */}
-      <div className="mb-6">
-        <div className="flex items-center border-2 border-gray-200 rounded">
-          <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+      <div className='mb-6'>
+        <div className='flex items-center border-2 border-gray-200 rounded'>
+          <SearchIcon className='w-5 h-5 text-gray-500 m-2' />
           <input
-            className="w-full py-2 px-4 rounded bg-white"
-            placeholder="Search products..."
+            className='w-full py-2 px-4 rounded bg-white'
+            placeholder='Search products...'
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -42,39 +58,39 @@ const Products = () => {
       </div>
 
       {/* HEADER BAR */}
-      <div className="flex justify-between items-center mb-6">
-        <Header name="Products" />
+      <div className='flex justify-between items-center mb-6'>
+        <Header name='Products' />
         <button
-          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-boldv px-2 py-4 rounded"
+          className='flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-boldv px-2 py-4 rounded'
           onClick={() => setIsModalOpen(true)}
         >
-          <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
+          <PlusCircleIcon className='w-5 h-5 mr-2 !text-gray-200' /> Create
           Product
         </button>
       </div>
 
       {/* BODY PRODUCT LIST — scrollable */}
-      <div className="overflow-y-auto flex-1">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between">
+      <div className='overflow-y-auto flex-1'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between'>
           {isLoading ? (
             <div>Loading...</div>
           ) : (
             products?.map((product) => (
               <div
                 key={product.productId}
-                className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
+                className='border shadow rounded-md p-4 max-w-full w-full mx-auto'
               >
-                <div className="flex flex-col items-center">
+                <div className='flex flex-col items-center'>
                   img
-                  <h3 className="text-lg text-gray-900 font-semibold">
+                  <h3 className='text-lg text-gray-900 font-semibold'>
                     {product.name}
                   </h3>
-                  <p className="text-gray-800">${product.price.toFixed(2)}</p>
-                  <div className="text-sm text-gray-600 mt-1">
+                  <p className='text-gray-800'>${product.price.toFixed(2)}</p>
+                  <div className='text-sm text-gray-600 mt-1'>
                     Stock:{product.stockQuantity}
                   </div>
                   {product.rating && (
-                    <div className="flex items-center mt-2">
+                    <div className='flex items-center mt-2'>
                       <Rating rating={product.rating} />
                     </div>
                   )}
@@ -87,6 +103,11 @@ const Products = () => {
       </div>
 
       {/* MODAL */}
+      <CreateProductModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onCreate={handleCreateProduct}
+      />
     </div>
   );
 };
